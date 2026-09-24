21c594709ffb964d2a2266d3444f6d5e1315946e
bruna <bruna682m@gmail.com>
2026-09-24 12:05:14 -0300

    reestruturação + arrumando meta

--- 11_reestruturacao.py
+++ 11_reestruturacao.py
@@ -3,6 +3,8 @@ import datetime
 
 historico_dias_estudados = []
 
+dias_meta = 0
+
 arquivo = open("teste.txt", "a")
 arquivo.close()
 
@@ -12,6 +14,23 @@ linhas = arquivo.readlines()
 
 arquivo.close()
 
+arquivo_meta = open("meta.txt", "a")
+arquivo_meta.close()
+
+arquivo_meta = open("meta.txt", "r")
+
+linha_meta = arquivo_meta.readlines()
+
+arquivo_meta.close()
+
+arquivo_progresso = open("progresso_meta.txt", "a")
+arquivo_progresso.close()
+
+arquivo_progresso = open("progresso_meta.txt", "r")
+linha_progresso = arquivo_progresso.readlines()
+arquivo_progresso.close()
+
+
 for linha in linhas:
     linha = linha.strip()
 
@@ -19,7 +38,26 @@ for linha in linhas:
 
     historico_dias_estudados.append(data)
 
-meta = 30
+if linha_meta == []:
+
+    while True:
+        meta = int(input("Digite sua meta de dias de estudos:"))
+
+        if meta > 0:
+            break
+
+        print("A meta deve ser maior que 0.")
+
+    arquivo_meta = open("meta.txt", "w")
+    arquivo_meta.write(str(meta))
+    arquivo_meta.close()
+else:
+    meta = int(linha_meta[0])
+
+if linha_progresso == []:
+    dias_meta = 0
+else: 
+    dias_meta = int(linha_progresso[0])
 
 while True:
     data_atual = datetime.date.today()
@@ -30,8 +68,8 @@ while True:
 ======================================
 
 Ofensiva: {len(historico_dias_estudados)}
-Meta: 
-Faltam: 
+Meta: {meta}
+Faltam: {meta - len(historico_dias_estudados)}
 
 1- Registrar estudo
 2- Ver ofensiva
@@ -47,6 +85,8 @@ Faltam:
             print("Você já registrou o estudo de hoje.")
         else:
             historico_dias_estudados.append(data_atual)
+            dias_meta += 1
+
             print("Dias estudados:", len(historico_dias_estudados))
 
             arquivo = open("teste.txt", "a")
@@ -62,7 +102,6 @@ Faltam:
             if historico_dias_estudados == []:
                 print("Ainda não há estudos registrados.")
             else:
-                data_atual = datetime.date.today()
                 sequencia = 0
 
                 dia_anterior = data_atual + datetime.timedelta(days= -1)
@@ -90,10 +129,57 @@ Faltam:
                 print("Sequencia atual:",sequencia, "dia(s)")
 
     elif opcao == 3:
-     print("ver historico")
+        print("Hístórico de estudos:")
+
+        if historico_dias_estudados == []:
+            print("Ainda não há estudos registrados.")
+        
+        for data in historico_dias_estudados:
+            data_formatada = data.strftime("%d/%m/%Y")
+            print(data_formatada)
 
     elif opcao == 4:
-     print("Ver meta")
+        print("Ver meta")
+
+        faltam = meta - dias_meta
+
+        if dias_meta >= meta:
+            faltam = 0
+
+        progresso = dias_meta / meta * 100
+        
+        if progresso >= 100:
+            progresso = 100
+
+
+        print(f"""
+======================================
+              Meta
+======================================
+Meta: {meta} dias
+Días estudados: {dias_meta}
+Faltam: {faltam} dias
+Progresso: {progresso:.1f}%
+""")
+     
+        if dias_meta >= meta:
+            print("Meta atinginda!")
+
+            while True:
+                meta = int(input("Digite sua nova meta de dias de estudos:"))
+
+                if meta > 0:
+                    break
+
+                print("A nova meta dever ser maior que 0.")
+
+            dias_meta = 0
+
+            arquivo_meta = open("meta.txt", "w")
+            arquivo_meta.write(str(meta))
+            arquivo_meta.close
+        else:
+            print("Continue estudando!")
                 
     elif opcao == 5:
         print("Saindo...")
